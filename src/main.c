3a93f26325fd3df8b521621525cab074e34f5294
Ramazan Aslan <ramazanaslan21901@gmail.com>
2024-09-27 13:32:43 +0300

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <libxfce4util/libxfce4util.h>
+#include <libxfce4ui/libxfce4ui.h>
+
+#include <libxfce4panel/libxfce4panel.h>
+#include <gtk/gtk.h>
+
+static void
+prayer_times_construct(XfcePanelPlugin *plugin)
+{
+
+}
+
+XFCE_PANEL_PLUGIN_REGISTER(prayer_times_construct)
